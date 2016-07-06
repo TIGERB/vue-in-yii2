bf54841fdc1d9667b048d6128efb7326c005f285
TIGERB <624661874@qq.com>
2016-07-06 23:40:15 +0800

    fix list in v1/testController/actionList

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<link href="./dist/app.css" rel="stylesheet"></head>
+<link href="http://vue-in-yii2.local/dist/app.css" rel="stylesheet"></head>
 <body>
 <div id='app'></div>
-<script type="text/javascript" src="./dist/app.js"></script></body>
+<script type="text/javascript" src="http://vue-in-yii2.local/dist/app.js"></script></body>
 </html>
